6a690000080a114768977b178d7cd8eb22dbd353
Bhargavi Kullakanda <bhargavi.kullakanda.ext@bayer.com>
2024-08-08 22:54:37 -0500

    View to show weather data

--- src/components/WeatherDisplayView.tsx
+++ src/components/WeatherDisplayView.tsx
@@ -1,3 +1,104 @@
-export const WeatherDisplayView = (): JSX.Element => {
-  return <></>;
+import {ScrollView, StyleSheet, Text, useColorScheme, View} from 'react-native';
+import {Colors} from 'react-native/Libraries/NewAppScreen';
+import React, {PropsWithChildren} from 'react';
+import {WeatherDisplayData} from '../types/weatherDisplayData';
+
+const styles = StyleSheet.create({
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400',
+  },
+  highlight: {
+    fontWeight: '700',
+  },
+});
+
+type SectionProps = PropsWithChildren<{
+  title: string;
+  testID: string;
+}>;
+
+function Section({children, title, testID}: SectionProps): React.JSX.Element {
+  const isDarkMode = useColorScheme() === 'dark';
+  return (
+    <View style={styles.sectionContainer} testID={testID}>
+      <Text
+        style={[
+          styles.sectionTitle,
+          {
+            color: isDarkMode ? Colors.white : Colors.black,
+          },
+        ]}>
+        {title}
+      </Text>
+      <Text
+        style={[
+          styles.sectionDescription,
+          {
+            color: isDarkMode ? Colors.light : Colors.dark,
+          },
+        ]}>
+        {children}
+      </Text>
+    </View>
+  );
+}
+interface WeatherDisplayProps {
+  weatherDisplayData: WeatherDisplayData | undefined;
+}
+export const WeatherDisplayView = ({
+  weatherDisplayData,
+}: WeatherDisplayProps): JSX.Element => {
+  const isDarkMode = useColorScheme() === 'dark';
+  const backgroundStyle = {
+    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  };
+
+  return (
+    <ScrollView
+      contentInsetAdjustmentBehavior="automatic"
+      style={backgroundStyle}>
+      {weatherDisplayData ? (
+        <View
+          style={{
+            backgroundColor: isDarkMode ? Colors.black : Colors.white,
+          }}
+          testID={'weatherDataView'}>
+          <Section title="Current Location" testID={'locationSection'}>
+            {weatherDisplayData.locationName}
+          </Section>
+          <Section title="Weather Condition" testID={'weatherConditionSection'}>
+            {weatherDisplayData.conditions}
+          </Section>
+          <Section title="Current Temperature" testID={'currentTempSection'}>
+            {`${weatherDisplayData.currentTemp}\u00B0`}
+          </Section>
+          <Section
+            title="Feels Like"
+            testID={
+              'feelsLikeTempSection'
+            }>{`${weatherDisplayData.feels_like}\u00B0`}</Section>
+          <Section
+            title="High"
+            testID={
+              'maxTempSection'
+            }>{`${weatherDisplayData.temp_max}\u00B0`}</Section>
+          <Section
+            title="Low"
+            testID={
+              'minTempSection'
+            }>{`${weatherDisplayData.temp_min}\u00B0`}</Section>
+        </View>
+      ) : null}
+    </ScrollView>
+  );
 };
---
+++ tests/src/components/WeatherDisplayView.test.tsx
@@ -0,0 +1,68 @@
+import {render, screen} from '@testing-library/react-native';
+import {WeatherDisplayView} from '../../../src/components/WeatherDisplayView';
+
+describe('WeatherDisplayView', () => {
+  const weatherData = {
+    locationName: 'Little Elm',
+    currentTemp: 100,
+    feels_like: 103,
+    temp_min: 78,
+    temp_max: 106,
+    conditions: 'Cloudy',
+  };
+
+  it('shows view if weather data is passed in', async () => {
+    render(<WeatherDisplayView weatherDisplayData={weatherData} />);
+    const weatherDataView = await screen.findByTestId('weatherDataView');
+    expect(weatherDataView).toBeOnTheScreen();
+  });
+
+  it('shows correct location name', async () => {
+    render(<WeatherDisplayView weatherDisplayData={weatherData} />);
+    const section = await screen.findByTestId('locationSection');
+
+    expect(section).toBeOnTheScreen();
+    expect(section).toHaveTextContent('Current Location', {exact: false});
+    expect(section).toHaveTextContent(weatherData.locationName, {exact: false});
+  });
+
+  it('shows correct weather condition', async () => {
+    render(<WeatherDisplayView weatherDisplayData={weatherData} />);
+    const section = await screen.findByTestId('weatherConditionSection');
+    expect(section).toBeOnTheScreen();
+    expect(section).toHaveTextContent('Weather Condition', {exact: false});
+    expect(section).toHaveTextContent(weatherData.conditions, {exact: false});
+  });
+
+  it('shows correct current temperature', async () => {
+    render(<WeatherDisplayView weatherDisplayData={weatherData} />);
+    const section = await screen.findByTestId('currentTempSection');
+    expect(section).toBeOnTheScreen();
+    expect(section).toHaveTextContent('Current Temperature', {exact: false});
+    expect(section).toHaveTextContent('100\u00B0', {exact: false});
+  });
+
+  it('shows correct feels like temperature', async () => {
+    render(<WeatherDisplayView weatherDisplayData={weatherData} />);
+    const section = await screen.findByTestId('feelsLikeTempSection');
+    expect(section).toBeOnTheScreen();
+    expect(section).toHaveTextContent('Feels Like', {exact: false});
+    expect(section).toHaveTextContent('103\u00B0', {exact: false});
+  });
+
+  it('shows correct max temperature', async () => {
+    render(<WeatherDisplayView weatherDisplayData={weatherData} />);
+    const section = await screen.findByTestId('maxTempSection');
+    expect(section).toBeOnTheScreen();
+    expect(section).toHaveTextContent('High', {exact: false});
+    expect(section).toHaveTextContent('106\u00B0', {exact: false});
+  });
+
+  it('shows correct min temperature', async () => {
+    render(<WeatherDisplayView weatherDisplayData={weatherData} />);
+    const section = await screen.findByTestId('minTempSection');
+    expect(section).toBeOnTheScreen();
+    expect(section).toHaveTextContent('Low', {exact: false});
+    expect(section).toHaveTextContent('78\u00B0', {exact: false});
+  });
+});
